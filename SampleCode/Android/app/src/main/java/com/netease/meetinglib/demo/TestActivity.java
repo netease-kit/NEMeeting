1f8bec7a5f2ae6685536d5cd02d2e8c13cd2ad02
zhaochong <zhaochong01@corp.netease.com>
2020-09-04 18:52:36 +0800

    feat:增加空页面编译文件

---
+++ SampleCode/Android/app/src/main/java/com/netease/meetinglib/demo/TestActivity.java
@@ -0,0 +1,20 @@
+package com.netease.meetinglib.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+//关闭当前测试界面
+    public void finish(View view) {
+       finish();
+    }
+}

22ba6551f351c63d0c1de23654dd3e97b863623e
Groot <guoyuanyuan02@corp.netease.com>
2020-09-29 20:10:53 +0800

    update Config.m

--- SampleCode/iOS/NEMeetingDemo/Config.m
+++ SampleCode/iOS/NEMeetingDemo/Config.m
@@ -8,14 +8,14 @@
 
 #import "Config.h"
 
-NSString *const kAppKey = @"请填入您的AppKey";
+NSString *const kAppKey = @"<#请填入您的AppKey#>";
 
 @implementation Config
 
 + (void)queryAccountInfoWithUserName:(NSString *)userName
                             password:(NSString *)password
                           completion:(QueryAccoutInfoBlock)completion {
-    // 请根据自己情况获得真正的用户id和token,并调用completion返回
+    <#请根据自己情况获得真正的用户id和token,并调用completion返回#>
 }
 
 @end

a91607c4d67a6d08341b3e3a6160dfdcded5acba
zhaochong <zhaochong01@corp.netease.com>
2024-08-19 14:44:40 +0800

    feat：配置QT 应用权限示例

--- SampleCode/Windows_macOS/meeting_sdk_example_qt/build.bat
+++ SampleCode/Windows_macOS/meeting_sdk_example_qt/build.bat
@@ -3,10 +3,11 @@ if exist "meeting_sdk_example_qt" (
     rmdir /s /q "meeting_sdk_example_qt"
 )
 
+:: 设置你的QT_PATH
 :: Qt环境替换成本地Qt环境
 cmake   -Bmeeting_sdk_example_qt -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_PREFIX_PATH=D:\Qt\6.7.2\msvc2019_64
 cmake   --build meeting_sdk_example_qt --config Release
 
 xcopy /E /I /Y "SDK\BIN\*" "..\meeting_sdk_example_qt\Release\"
-set QT_windeployqt=%QT_PLUGIN_PATH%\..\..\bin\windeployqt.exe
+set QT_windeployqt=D:\Qt\6.7.2\msvc2019_64\bin\windeployqt.exe
 call %QT_windeployqt% .\meeting_sdk_example_qt\Release\meeting_sdk_example_qt.exe
--- SampleCode/Windows_macOS/meeting_sdk_example_qt/build.sh
+++ SampleCode/Windows_macOS/meeting_sdk_example_qt/build.sh
@@ -3,10 +3,6 @@
 rm -rf meeting_sdk_example_qt
 #Qt环境替换成本地Qt环境
 Qt=~/Qt/6.5.3/macos
-#cmake   -Bmeeting_sample \
-#        -DCMAKE_BUILD_TYPE=Release \
-#        -DCMAKE_OSX_ARCHITECTURES=x86_64 \
-#        -DCMAKE_PREFIX_PATH=~/meeting_sdk_example_qt/6.5.3/macos
 
 cmake   -Bmeeting_sdk_example_qt \
         -DCMAKE_BUILD_TYPE=Release \
@@ -17,7 +13,7 @@ cmake   -Bmeeting_sdk_example_qt \
 cmake   --build meeting_sdk_example_qt --config Release
 
 # 如果需要打包，则需要执行以下命令：并且要签名
-# ${Qt}/bin/macdeployqt6 meeting_sdk_example_qt/meeting_sdk_example_qt.app
+${Qt}/bin/macdeployqt6 meeting_sdk_example_qt/meeting_sdk_example_qt.app
 # codesign --timestamp --options=runtime -f -s "你的签名ID" -v meeting_sdk_example_qt/meeting_sdk_example_qt.app --deep
 # 启动Sample
 ./meeting_sdk_example_qt/meeting_sdk_example_qt.app/Contents/MacOS/meeting_sdk_example_qt

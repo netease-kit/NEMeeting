d60ac0eb5c67c6ec66e87b274ceb34b4bb9d8955
ChandarLee <chandarlee@users.noreply.github.com>
2026-01-13 15:31:07 +0800

    fix: 优化 windows 跑通文档 (#272)
    * fix: 优化文档结构
    
    * fix: 优化windows构建
    
    ---------
    
    Co-authored-by: hzlichengda <hzlichengda@corp.netease.com>

--- SampleCode/Desktop/meeting_sdk_example_qt/build_win.bat
+++ SampleCode/Desktop/meeting_sdk_example_qt/build_win.bat
@@ -10,6 +10,8 @@ set QT_PATH=D:\Qt\6.7.2\msvc2019_64
 cmake -Bbuild -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_PREFIX_PATH=%QT_PATH%
 cmake --build build --config Release
 
-xcopy /E /I /Y "SDK\BIN\*" "..\build\Release\"
+:: 复制SDK\BIN\下的所有文件到 build\Release\
+xcopy /E /I /Y "SDK\BIN\*" ".\build\Release\"
 set QT_windeployqt=%QT_PATH%\bin\windeployqt.exe
 call %QT_windeployqt% .\build\Release\meeting_sdk_example_qt.exe
+.\build\Release\meeting_sdk_example_qt.exe
